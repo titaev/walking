fa49a6eb64793add3023deb8cfc9979910307358
DmitriyAlekseyev <21g@mail.ru>
2022-06-04 18:35:45 +0300

    Django first step

--- walking/walking/settings.py
+++ walking/walking/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%r#tl)5kzbgic-@+4xbt%canbt5qy!sa+pk+8^lu89!uf7)u@!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
