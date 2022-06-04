146a853799e0adddb429e956171c16b45b3159f6
brun58 <brun58@ya.ru>
2022-06-04 15:52:00 +0300

    start

--- walking/walking/settings.py
+++ walking/walking/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wbg&qj0k^t=ehna#0x0#_4$3%qiq+_8papcbt5#&ktdfmi0m*h'
+SECRET_KEY = 'django-insecure-wbg&qj0k^t=ehna#0x0#_4$3%qiq+_8papcbt5#&ktdfmidfgjhfgh0m*h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main'
 ]
 
 MIDDLEWARE = [
