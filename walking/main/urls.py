c33c01c6a0ba61931bfdde942e94f54bff9b3236
FirstTi <ekaterinmax1mova@yandex.ru>
2022-06-05 19:08:52 +0300

    2

--- walking/main/urls.py
+++ walking/main/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path
-
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
@@ -11,3 +12,7 @@ urlpatterns = [
     # path('second/', views.second_page, name='second'),
     # path('second/another/', views.another_page, name='another')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
--- walking/main/views.py
+++ walking/main/views.py
@@ -1,21 +1,22 @@
 from django.shortcuts import render
-from .models import City
+from .models import City, Walk
 
 
 def index(request):
     # page = requests.get('https://xn----7sbiew6aadnema7p.xn--p1ai/alphabet.php')
     # soup = BeautifulSoup(page.text, 'html.parser')
-    #
     # result = soup.find('div', {'class': "common-text"}).find('ul').find_all('li')
     # result = [element.text for element in result]
     result = City.objects.all()
-    result = [element.name for element in result]
+    # result = [element.name for element in result]
     return render(request, 'main/index.html', {'cities': result})
 
 
 def walk(request):
-    city = request.GET.get('city')
-    return render(request, 'main/walk.html', {"city": city})
+    city_id = request.GET.get('city')
+    walk = Walk.objects.get(id=city_id)
+    return render(request, 'main/walk.html', {"walk": walk})
+
 
 def about_site(request):
     return render(request, 'main/FAQ.html')
