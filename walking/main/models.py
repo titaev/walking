e69fdd5816ae029b512a458d4136fd1f680933bd
brun58 <brun58@ya.ru>
2022-06-05 18:23:00 +0300

    add skelet site

--- walking/main/models.py
+++ walking/main/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+
+class City(models.Model):
+    name = models.CharField(max_length=300)
+
+
+class Walk(models.Model):
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    distance = models.FloatField()
+    description = models.TextField()
+    map = models.FileField(upload_to='maps/')
+
+
+class WalkImage(models.Model):
+    walk = models.ForeignKey(Walk, on_delete=models.CASCADE)
+    image = models.FileField(upload_to='walks/')
+
+
+class Review(models.Model):
+    walk = models.ForeignKey(Walk, on_delete=models.CASCADE)
+    rating = models.IntegerField(blank=True)
+    description = models.TextField(blank=True)
--- walking/main/views.py
+++ walking/main/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from .services import get_all_cities
+from .models import City
 
 
 def index(request):
-    result = get_all_cities()
+    result = City.objects.all()
+    result = [element.name for element in result]
     return render(request, 'main/index.html', {'cities': result})
 
 
