db1b315f4818e92853f5cac0af7ec76fba72eb47
Камиль Исхаков <82610019+KamilIskhakov@users.noreply.github.com>
2022-06-05 18:54:49 +0300

    12

--- walking/main/views.py
+++ walking/main/views.py
@@ -1,18 +1,20 @@
 from django.shortcuts import render
 # from .services import get_all_cities
-from .models import City
+from .models import City, Walk
 
 def index(request):
     # result = get_all_cities()
     result = City.objects.all()
-    result = [element.name for element in result]
+    # result = [element.name for element in result]
+
     return render(request, 'main/index.html', {'cities': result})
 
 
 def walk(request):
     city = request.GET.get('city')
+    walk = Walk.objects.get(city=city_id)
     # здесь делаем запрос в базу
-    return render(request, 'main/walk.html', {"city": city})
+    return render(request, 'main/walk.html', {"walk": walk})
 
 
 
