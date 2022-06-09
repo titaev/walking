02870411596dc90db9c7e44209e60e85dce8c084
ititaev <i.titaev@ringme.ru>
2022-06-09 11:55:15 +0300

    stable images

--- walking/walking/settings.py
+++ walking/walking/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 # Base url to serve media files
-MEDIA_URL = 'walk/media/'
+MEDIA_URL = 'media/'
 
 # Path where media is stored
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
