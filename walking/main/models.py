a364282eaefcb269df5310778e72e49ff7e5b3f9
ititaev <i.titaev@ringme.ru>
2022-06-09 12:31:27 +0300

    register stable

--- walking/main/models.py
+++ walking/main/models.py
@@ -6,6 +6,9 @@ from django.db import models
 class City(models.Model):
     name = models.CharField(max_length=300)
 
+    def __str__(self):
+        return self.name
+
 
 class Walk(models.Model):
     city = models.ForeignKey(City, on_delete=models.CASCADE)
