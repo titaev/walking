1e91aa1a206d36372d246091d1a645917008c71d
FirstTi <ekaterinmax1mova@yandex.ru>
2022-06-11 16:18:39 +0300

    add

--- walking/walking/settings.py
+++ walking/walking/settings.py
@@ -120,7 +120,7 @@ STATIC_URL = 'static/'
 
 MEDIA_URL = 'media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, '/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 
 # Default primary key field type
